773def0137b836222541134c792598c689f93ad5
Amitesh kumar <122549839+amitesh00272@users.noreply.github.com>
2023-06-16 20:46:51 +0530

    Add files via upload

---
+++ discovery_game.py
@@ -0,0 +1,20 @@
+class Discovery_game:
+    @staticmethod
+    def first_level():
+        print("<<<<Hello Everyone! Welcome in this game>>>>\
+              I am Amitesh Kumar and i made this game for Jassos")
+
+
+    @staticmethod
+    def second_level():
+        pass
+
+    @staticmethod
+    def final_level():
+        pass
+
+
+if __name__ == '__main__':
+    Amitesh_Kumar = Discovery_game()
+    Amitesh_Kumar.first_level()
+    
